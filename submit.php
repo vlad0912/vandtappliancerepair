7ed65269495ba121f490cf5c0752093efd1f4044
vlad0912 <60529161+vlad0912@users.noreply.github.com>
2020-01-31 16:35:22 -0800

    Add files via upload

---
+++ submit.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+ <html>
+  <head>
+    <meta charset="utf-8">
+    <title>Service Request Form</title>
+    <link
+    href="https://fonts.googleapis.com/css?family=Roboto+condensed:400,700"
+    rel="stylesheet">
+    <link rel="stylesheet" href="submitstyle.html">
+  </head>
+  <body>
+    <main>
+     <p> Submit your service requests. </p>
+    <form class="services-form" action="submit.php" method="post">
+   <input type="text" name="Name" placeholder="Name">
+   <input type="text" name="" placeholder="">
+   <input type="text" name="" placeholder="">
+   <input type="text" name="" placeholder="">
+   <input type="text" name="" placeholder="">
+   <textarea name= "Problem Description" placeholder="Problem Description"> </textarea>
+   <button type="submit" name="submit"> Submit service requests </button>
+  </form>
+    </main>
+   </body>
+  </html>
